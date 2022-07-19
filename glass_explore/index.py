53a98aa141ed33b0400f6b42391cbcfe4ca9bbb8
Jon Robinson <jonrobinson1980@gmail.com>
2022-07-19 19:46:22 +1200

    manufacturers plugged in

--- glass_explore/index.py
+++ glass_explore/index.py
@@ -8,20 +8,22 @@ import plotly.express as px
 import pandas as pd
 from glass_explore import utils
 
-data = 'csv'
+DATA_SOURCE = 'csv'
 
-if data == 'sql':
+ALL = '[ALL]'
+
+if DATA_SOURCE == 'sql':
     cxn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=F:\dev\glass-explore\.venv\data\test.mdb;')
     sql = 'select * from Glass where thickness > 5.5 and thickness < 6.5'
     df = pd.read_sql(sql,cxn)
-elif data == 'csv':
+elif DATA_SOURCE == 'csv':
     path = os.path.join('data', 'igdb.csv')
     raw_df = pd.read_csv(path, encoding='ISO-8859-1')
     df = raw_df[raw_df['Thickness'].between(5.5, 6.5)]
     
 
 manufacturers = np.sort(df.Manufacturer.unique())
-manufacturers = np.insert(manufacturers,0,"[ALL]")
+manufacturers = np.insert(manufacturers,0,ALL)
 
 
 df['Color'] = df['Color'].map(lambda x:utils.base10color_to_csshex(x))
@@ -41,7 +43,7 @@ def query_data():
 
 
 manufacturer_select = html.Div([
-    dbc.Label("Choose manufacturer:"),
+    dbc.Label("Manufacturer:"),
     dbc.Select(
         id="select-manufacturer",
         options=[{"label": m, "value": m} for m in manufacturers]
@@ -59,18 +61,21 @@ app.layout = dbc.Container([
     Output("graph", "figure"),
     [
         Input("select-manufacturer", "value"),
-    ],
-    [
-        State("graph", "figure")
     ]
 )
-def on_filter_change(manufacturer, figure):
-    print(manufacturer)
-    figure.update_traces(marker=dict(color = df['Color'],
-                            line=dict(width=2,
-                                        color='DarkSlateGrey')),
-                    selector=dict(mode='markers'))
-    return figure
+def on_filter_change(manufacturer):
+
+    if manufacturer == ALL:
+        fig = px.scatter(df, x="Tsol", y="Tvis",  hover_data= ["ID","Manufacturer", "ProductName"])
+    else:
+        mask = (df['Manufacturer'] == manufacturer)
+        fig = px.scatter(df[mask], x="Tsol", y="Tvis",  hover_data= ["ID","Manufacturer", "ProductName"])
+    
+
+    fig.layout.plot_bgcolor = 'white'
+    fig.update_traces(marker=dict(color = df['Color']),
+                    selector=dict(mode='markers'))   
+    return fig
 
 
 
