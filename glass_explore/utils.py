038321a0631953ee0ea4cf5ce9f3c889ee701f7b
Jon Robinson <jonrobinson1980@gmail.com>
2022-07-20 15:28:13 +1200

    remove old hex fn

--- glass_explore/utils.py
+++ glass_explore/utils.py
@@ -1,23 +1,3 @@
-def base10color_to_csshex_old(x: int, reverse  : bool = True) -> str:
-    """
-    IGDB stores 'color' as a 24-bit color code (int between 0 to 16.8million).
-    This returns a css hex string format, eg '#ffffff' for white.
-
-    Args:
-        x (int): 24bit colour code
-        reverse (bool) : swaps 'Red' and 'Blue', eg #c93658 -> #5836c9
-
-    Returns:
-        str: hex str starting with '#' and always 7 chars.
-    """
-    s = '#' + str("{0:#0{1}x}".format(x,6))[2:]
-    if len(s)<7:
-        s = s[0:5] + '0' + s[-1]
-    if reverse:
-        s = s[0] + s[-2:] + s[3:5]  + s[1:3]  
-    return s
-
-
 HEX_LETTER = ['a','b','c','d','e','f']
 LITTLE_ENDIAN_ORDER = [1,0,3,2,5,4]
 
