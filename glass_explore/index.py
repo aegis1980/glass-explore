e2a3fb165ab782866ad31c4653ab848b62bb51be
Jon Robinson <jonrobinson1980@gmail.com>
2022-07-20 15:27:46 +1200

    tidied up and corrected glass colour

--- glass_explore/index.py
+++ glass_explore/index.py
@@ -1,9 +1,11 @@
+from tkinter.tix import ListNoteBook
 import pyodbc
 import os
 import dash
 from dash import dcc,html, Input, Output, State
 import dash_bootstrap_components as dbc
 import numpy as np
+import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
 from glass_explore import utils
@@ -19,66 +21,201 @@ if DATA_SOURCE == 'sql':
 elif DATA_SOURCE == 'csv':
     path = os.path.join('data', 'igdb.csv')
     raw_df = pd.read_csv(path, encoding='ISO-8859-1')
-    df = raw_df[raw_df['Thickness'].between(5.5, 6.5)]
-    
+    raw_df = raw_df[raw_df['Thickness'].between(5.5, 8.5)]
 
-manufacturers = np.sort(df.Manufacturer.unique())
+manufacturers = np.sort(raw_df.Manufacturer.unique())
 manufacturers = np.insert(manufacturers,0,ALL)
 
+# IGDB uses nnumber for color, we waht CSS hex value.
+raw_df['CssColor'] = raw_df['Color'].map(lambda x:utils.base10color_to_csshex(x))
 
-df['Color'] = df['Color'].map(lambda x:utils.base10color_to_csshex(x))
-fig = px.scatter(df, x="Tsol", y="Tvis",  hover_data= ["ID","Manufacturer", "ProductName"])
-fig.update_traces(marker=dict(color = df['Color']),
-                selector=dict(mode='markers'))
+fig = go.Figure()
+fig.update_layout(
+    height = 800,
+)
 
-fig.layout.plot_bgcolor = 'white'
+modal_popup = dbc.Modal(
+            [
+                dbc.ModalHeader(dbc.ModalTitle("Welcome to Glass Explore")),
+                dbc.ModalBody([
+                    html.P("Data from Lawrence Berkeley National Laboratory IDGB database."),
+                    html.P(["Chart plots Solar Transmittance (T",html.Sub("sol"),") and Visible Light Transmittance  (T",html.Sub("vis"),") of 6mm and 8mm glasses in the database."]),
+                    html.P(["Note:  Solar and visilble light transmission properties will depend on what glazing buildup these products are included in. The charted (T",html.Sub("vis"),") and (T",html.Sub("sol"),") are only indicative of the VLT and g-factor/SHGC of the buildup performance."])
+                ]),
+                dbc.ModalFooter(
+                    dbc.Button(
+                        "Close", id="close", className="ms-auto", n_clicks=0
+                    )
+                ),
+            ],
+            id="modal",
+            is_open=True,
+        )
 
-def query_data():
-    df = pd.read_sql(sql,cxn)
-    df['Color'] = df['Color'].map(lambda x:utils.base10color_to_csshex(x))
-    fig = px.scatter(df, x="Tsol", y="Tvis",  hover_data= ["ID","Manufacturer", "ProductName"])
-    fig.update_traces(marker=dict(color = df['Color']),
-                    selector=dict(mode='markers'))
-
-
-
-manufacturer_select = html.Div([
+select_manufacturer = html.Div([
     dbc.Label("Manufacturer:"),
     dbc.Select(
-        id="select-manufacturer",
+        id="select-manufacturer", value = ALL,
         options=[{"label": m, "value": m} for m in manufacturers]
-    )
+    ),
+    dbc.FormText(id = 'formtext-manufacturer',color='red'),
 ])
 
+radio_thickness = html.Div([
+    dbc.Label("Substrate thickness:"),
+     dbc.RadioItems(
+            options=[
+                {"label": "6mm", "value": 6},
+                {"label": "8mm", "value": 8},
+            ],
+            value=6,
+            id="radio-thickness",
+        ),
+    ]
+)
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+    ],
+)
 app.layout = dbc.Container([
-    manufacturer_select,
-    dcc.Graph(id = "graph", figure=fig)
+    dbc.Row([
+        dbc.Col(select_manufacturer),
+        dbc.Col(radio_thickness)
+    ]),
+    dcc.Graph(id = "graph", figure = fig),
+    modal_popup
 ])
 
 @app.callback(
-    Output("graph", "figure"),
+    Output("graph", "figure"),Output("formtext-manufacturer","children"),Output("formtext-manufacturer","color"),
     [
         Input("select-manufacturer", "value"),
+        Input("radio-thickness", "value"),
     ]
 )
-def on_filter_change(manufacturer):
+def on_filter_change(manufacturer, thickness):
 
+    df = raw_df[raw_df['Thickness'].between(thickness - 0.5, thickness + 0.5)]
+
+    fig = go.Figure()
     if manufacturer == ALL:
-        fig = px.scatter(df, x="Tsol", y="Tvis",  hover_data= ["ID","Manufacturer", "ProductName"])
+
+        msg = f'{len(df.index)} glasses'
+        color = 'darkgrey'
+        fig.add_trace(
+            go.Scatter(
+                mode='markers',
+                x=df["Tsol"],
+                y=df["Tvis"],
+                customdata=df,
+                marker=dict(
+                    color=df['CssColor'],
+                    size=10,
+                ),
+
+                showlegend=False,
+                hovertemplate = 
+                    '<b>id</b>: %{customdata[0]}' + 
+                    '<br>(<b>T_v</b>: %{y:.2f}' + ' <b>T_s</b>: %{x:.2f})'+
+                    '<br>%{customdata[17]}' + 
+                    '<br>%{customdata[18]}'
+            )
+        )
+       
     else:
+        mask_na = (df['Manufacturer'] != manufacturer)
         mask = (df['Manufacturer'] == manufacturer)
-        fig = px.scatter(df[mask], x="Tsol", y="Tvis",  hover_data= ["ID","Manufacturer", "ProductName"])
+
+        if len(df[mask].index)==0:
+            msg = f'No glasses from {manufacturer} with thickness, {thickness}mm'
+            color = 'red'
+        else:
+            msg = f'{len(df[mask].index)} glasses'
+            color = 'darkgrey'
+
+        fig.add_trace(
+            go.Scatter(
+                mode='markers',
+                x=df[mask_na]["Tsol"],
+                y=df[mask_na]["Tvis"],
+                marker=dict(
+                    color=df[mask_na]['CssColor'],
+                    opacity = 0.4,
+                    size=10,
+                ),
+                showlegend=False,
+                hoverinfo='skip'
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                mode='markers',
+                x=df[mask]["Tsol"],
+                y=df[mask]["Tvis"],
+                customdata=df[mask],
+                marker=dict(
+                    color=df[mask]['CssColor'],
+                    size=20,
+                    line=dict(
+                        color='Red',
+                        width=1
+                    )
+                ),
+                showlegend=False,
+                hovertemplate = 
+                    '<b>id</b>: %{customdata[0]}' + 
+                    '<br>(<b>T_v</b>: %{y:.2f}' + ' <b>T_s</b>: %{x:.2f})'+
+                    '<br>%{customdata[17]}' + 
+                    '<br>%{customdata[18]}'
+            )
+        )
+        
+        
+      #  fig = px.scatter(df[mask], x="Tsol", y="Tvis",  hover_data= ["ID","Manufacturer", "ProductName"])
     
+    fig.update_layout(
+        xaxis_title="T_solar",
+        yaxis_title="T_visible",
+        plot_bgcolor = "white",
+        hovermode = 'closest'
+    )
 
-    fig.layout.plot_bgcolor = 'white'
-    fig.update_traces(marker=dict(color = df['Color']),
-                    selector=dict(mode='markers'))   
-    return fig
+    fig.update_xaxes(
+        dtick=0.1, 
+        range=[0, 1],
+        zeroline = False,
+        fixedrange=True,
+        showgrid = True,
+        gridcolor='LightGrey',
+        minor=dict(showgrid=True)
+      #  autorange="reversed",
+      #  constrain="domain",  # meanwhile compresses the xaxis by decreasing its "domain"
+    ) 
+    fig.update_yaxes(
+        dtick=0.1, 
+        range=[0, 1],
+        zeroline = False,
+        fixedrange=True,
+        showgrid = True,
+        gridcolor='LightGrey',
+        minor=dict(showgrid=True)
+    )
 
+    return fig, msg, color
 
-
+@app.callback(
+    Output("modal", "is_open"),
+    [Input("close", "n_clicks")],
+    [State("modal", "is_open")],
+)
+def toggle_modal(n, is_open):
+    if n:
+        return not is_open
+    return is_open
 
 
 if __name__ == "__main__":
--- glass_explore/utils.py
+++ glass_explore/utils.py
@@ -1,10 +1,11 @@
-def base10color_to_csshex(x: int) -> str:
+def base10color_to_csshex_old(x: int, reverse  : bool = True) -> str:
     """
     IGDB stores 'color' as a 24-bit color code (int between 0 to 16.8million).
     This returns a css hex string format, eg '#ffffff' for white.
 
     Args:
-        x (int): 
+        x (int): 24bit colour code
+        reverse (bool) : swaps 'Red' and 'Blue', eg #c93658 -> #5836c9
 
     Returns:
         str: hex str starting with '#' and always 7 chars.
@@ -12,4 +13,35 @@ def base10color_to_csshex(x: int) -> str:
     s = '#' + str("{0:#0{1}x}".format(x,6))[2:]
     if len(s)<7:
         s = s[0:5] + '0' + s[-1]
+    if reverse:
+        s = s[0] + s[-2:] + s[3:5]  + s[1:3]  
     return s
+
+
+HEX_LETTER = ['a','b','c','d','e','f']
+LITTLE_ENDIAN_ORDER = [1,0,3,2,5,4]
+
+def base10color_to_csshex(x: int) -> str:
+    """
+    IGDB stores 'color' as a 24-bit color code (int between 0 to 16.8million).
+    This returns a css hex string format, eg '#ffffff' for white.
+
+    Args:
+        x (int): 24bit colour code
+
+    Returns:
+        str: hex str starting with '#' and always 7 chars.
+    """
+    h = ['0'] * 6 
+    for i in range(6):
+        
+        x1 = x % 16
+        x = x // 16
+        if x1>=10:
+            x1 = HEX_LETTER[x1-10]
+        h[LITTLE_ENDIAN_ORDER[i]] =  str(x1)   
+    return '#' + ''.join(h)
+
+
+if __name__ == "__main__":
+    print(base10color_to_csshex(628991))
